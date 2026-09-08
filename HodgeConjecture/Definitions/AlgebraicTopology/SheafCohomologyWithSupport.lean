521aa64b5eb17c169c458a015f5b2ffa70b3794f
Yaël Dillies <yael.dillies@gmail.com>
2026-09-08 17:30:33 +0200

    Remove extra spaces in notations

--- HodgeConjecture/Definitions/AlgebraicTopology/SheafCohomologyWithSupport.lean
+++ HodgeConjecture/Definitions/AlgebraicTopology/SheafCohomologyWithSupport.lean
@@ -67,10 +67,10 @@ namespace TopCat.Sheaf
 open _root_.Opens
 
 @[inherit_doc grothendieckTopology]
-notation "𝓖[ " X "]" => grothendieckTopology X
+notation "𝓖[" X "]" => grothendieckTopology X
 
 @[inherit_doc constantSheaf]
-notation3 "𝓒[ " Y " ; " A "]" => ((constantSheaf 𝓖[Y] AddCommGrpCat).obj A)
+notation3 "𝓒[" Y " ; " A "]" => (constantSheaf 𝓖[Y] AddCommGrpCat).obj A
 
 /-- Constant sheaves restrict along a continuous map. -/
 def constantRestriction {X Y : TopCat.{u}} (f : X ⟶ Y) (A : AddCommGrpCat.{u}) :
