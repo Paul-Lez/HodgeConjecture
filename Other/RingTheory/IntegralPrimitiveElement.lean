1ada141118d7796f0387a5adb8bd4b904370d923
Bhavik Mehta <bhavikmehta8@gmail.com>
2026-09-11 03:53:49 +0000

    Fix primitive element module author name

--- Other/RingTheory/IntegralPrimitiveElement.lean
+++ Other/RingTheory/IntegralPrimitiveElement.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2026 Bhargav Mehta. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Bhargav Mehta
+Authors: Bhavik Mehta
 -/
 module
 
