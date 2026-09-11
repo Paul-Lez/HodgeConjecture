b3883b155323f2df799c1d8859861ddf8ed3283e
Bhavik Mehta <bhavikmehta8@gmail.com>
2026-09-11 02:55:59 +0000

    Confine the sheaf-cohomology-with-support notations to their namespace

--- HodgeConjecture/Lemmas/AlgebraicTopology/SheafCohomologyWithSupport.lean
+++ HodgeConjecture/Lemmas/AlgebraicTopology/SheafCohomologyWithSupport.lean
@@ -68,10 +68,10 @@ namespace TopCat.Sheaf
 open _root_.Opens
 
 @[inherit_doc grothendieckTopology]
-notation "𝓖[" X "]" => grothendieckTopology X
+scoped notation "𝓖[" X "]" => grothendieckTopology X
 
 @[inherit_doc constantSheaf]
-notation3 "𝓒[" Y "; " A "]" => (constantSheaf 𝓖[Y] AddCommGrpCat).obj A
+scoped notation3 "𝓒[" Y "; " A "]" => (constantSheaf 𝓖[Y] AddCommGrpCat).obj A
 
 /-- Constant sheaves restrict along a continuous map. -/
 def constantRestriction {X Y : TopCat.{u}} (f : X ⟶ Y) (A : AddCommGrpCat.{u}) :
