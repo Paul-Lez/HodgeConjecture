839253c215de76cd69e4dda8f9fb9b74bc722250
Paul-Lez <paul.lezeau@gmail.com>
2026-09-25 09:36:37 +0000

    Use ordinary sheaf cohomology for integral Chern classes

--- Other.lean
+++ Other.lean
@@ -192,7 +192,6 @@ public import Other.RingTheory.AlgebraicNonvanishing
 public import Other.RingTheory.IntegralPrimitiveElement
 public import Other.RingTheory.RegularLocalQuotientKernel
 public import Other.RingTheory.StandardEtaleAlgebraic
-public import Other.AlgebraicGeometry.AnalyticSheafCohomologyExt
 public import Other.AlgebraicGeometry.DivisorOfRationalSection
 public import Other.AlgebraicGeometry.HolomorphicExponential
 public import Other.AlgebraicGeometry.HolomorphicExponentialSequence
